c84a2562804ed2e38c9892e785e5e3c58d93f8e9
marigorresen <110499531+marigorresen@users.noreply.github.com>
2022-08-23 09:05:43 -0300

    camera2D

--- PROBOT-DELL/KinematicBody2D.gd
+++ PROBOT-DELL/KinematicBody2D.gd
@@ -2,17 +2,18 @@ extends KinematicBody2D
 
 
 var velocity = Vector2.ZERO
-var move_speed = 480
+var move_speed = 280
 var gravity = 1200
 var jump_force = -320
 
 func _physics_process(delta: float) -> void:
-	velocity.y += gravity * delta
-	var move_direction = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
 	
-	if Input.is_action_pressed("ui_up"):
-		velocity.y = jump_force
+	var move_direction = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	var move_up_down = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
 	
 	velocity.x = move_speed * move_direction
+	velocity.y = move_speed * move_up_down
 	
-	move_and_slide(velocity)
+	velocity = move_and_slide(velocity)
+	
+	
