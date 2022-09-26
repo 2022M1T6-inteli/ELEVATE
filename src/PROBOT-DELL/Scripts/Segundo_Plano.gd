4b77fbba5e59b698803acfed8b7b19e3fc9f54cf
marigorresen <110499531+marigorresen@users.noreply.github.com>
2022-09-26 10:57:03 -0300

    bug fix pendrive

--- src/PROBOT-DELL/Scripts/Segundo_Plano.gd
+++ src/PROBOT-DELL/Scripts/Segundo_Plano.gd
@@ -3,6 +3,7 @@ extends Node2D
 onready var inventario= [get_node("Inventario/Inventariopng/PenDrive"), get_node("Inventario/Inventariopng/RAM"), get_node("Inventario/Inventariopng/Cd"), get_node("Inventario/Inventariopng/PapelSenha")]
 var PenDrive_visibility= false
 var contSfx = 0
+var contador = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -25,33 +26,28 @@ func _ready():
 	
 
 #Define a variavel de controle como true.
-func _on_RAM_Cena_body_entered(body):
+func _on_PenDrive_Cena_body_entered(body):
 	PenDrive_visibility= true
 
 #define variavel de controle como false. 
-func _on_RAM_Cena_body_exited(body):
+func _on_PenDrive_Cena_body_exited(body):
 	PenDrive_visibility= false
 
 
 func _physics_process(_delta):
-	if PenDrive_visibility == true and Input.is_action_just_pressed("ui_e") and $PenDrive.visible == true:
-		$PenDrive.visible= false
+	if PenDrive_visibility == true and Input.is_action_just_pressed("ui_e") and $PenDrive_Cena.visible == true:
+		$PenDrive_Cena.visible = false
 		inventario[0].visible= true
 		Global.Inventario_Itens[0]= inventario[0].visible
 		Global.contador = 6
+		contador = 1
 		if contSfx == 0: 
 			$itempegadosfx.play()
 		contSfx = contSfx+1
-	if Global.controlPenDrive == true:
-		$PenDrive_Cena.visible= true
+	if Global.controlPenDrive == true and inventario[0].visible == false:
+		$PenDrive_Cena.visible = true
 	
 	if Global.Inventario_Itens[0] == true:
 		Global.controlPenDrive= false
 
 
-func _on_PenDrive_Cena_body_entered(body):
-	pass # Replace with function body.
-
-
-func _on_PenDrive_Cena_body_exited(body):
-	pass # Replace with function body.
