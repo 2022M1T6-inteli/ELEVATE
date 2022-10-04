c05faa3bd5b4d2a98d48980ff0bd957f35b7a663
marigorresen <110499531+marigorresen@users.noreply.github.com>
2022-10-04 20:44:09 -0300

    correção hud

--- src/PROBOT-DELL/Scripts/OBJETIVO.gd
+++ src/PROBOT-DELL/Scripts/OBJETIVO.gd
@@ -2,7 +2,7 @@ extends ColorRect
 
 
 
-var vet_objetivos = ["Entre no predio","Fale com a Ellen","Va no primeiro andar","Fale com Marcos","Fale com a pinky no segundo andar","Pegue o Pen Drive","Conecte o Pen drive no computador do andar 1","Va no terceiro andar", "Fale com hawking", "Pegue a RAM", "Conserte o computador no segundo andar", "Fale com norman no quarto andar", "Pegue a placa de vídeo","Conserte o computador no terceiro andar"]
+var vet_objetivos = ["Entre no prédio","Fale com a Ellen","Vá ao primeiro andar","Fale com Marcos","Fale com a Pinky no segundo andar","Pegue o Pen Drive","Conecte o Pen drive no computador do primeiro andar","Vá ao terceiro andar", "Fale com Hawking", "Pegue a RAM", "Conserte o computador no segundo andar", "Fale com Norman no quarto andar", "Pegue a placa de vídeo","Conserte o computador no terceiro andar"]
 	
 	
 func _physics_process(_delta):
